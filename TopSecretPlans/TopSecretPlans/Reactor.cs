508db095f18a5c1bda2075bb6177c2ead13ba08b
Ryan Webb <dzanfox@gmail.com>
2016-01-19 11:54:46 -0600

    added IonDrive and reactor classes

---
+++ TopSecretPlans/TopSecretPlans/Reactor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSecretPlans
+{
+    public class Reactor
+    {
+        public int PowerRate { get; private set; }
+        Reactor(int PowerRate)
+        {
+            this.PowerRate = PowerRate;
+        }
+        public int GeneratePower()
+        {
+            return PowerRate;
+        }
+    }
+}
