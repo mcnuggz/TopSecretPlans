ebd3a039b18d4e104bb45bc012d923c7d72dfbf1
James Gille <jdgille@gmail.com>
2016-01-19 12:44:48 -0600

    no change in reactor

---
+++ TopSecretPlans/TopSecretPlans/MainPowerGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSecretPlans
+{
+	public class MainPowerGenerator : Reactor
+	{
+		public MainPowerGenerator(int PowerRate)
+		{
+			base.PowerRate = PowerRate;
+		}
+	}
+}
--- TopSecretPlans/TopSecretPlans/Reactor.cs
+++ TopSecretPlans/TopSecretPlans/Reactor.cs
@@ -15,6 +15,7 @@ namespace TopSecretPlans
         }
         public int GeneratePower()
         {
+			PowerRate++;
             return PowerRate;
         }
     }
