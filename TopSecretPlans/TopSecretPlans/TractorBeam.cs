5fcc83256a2338f106e91dae6f14da56c1e4b6ee
Your Name <you@example.com>
2016-01-19 13:38:04 -0600

    Tractor beam add

---
+++ TopSecretPlans/TopSecretPlans/TractorBeam.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSecretPlans
+{
+    public class TractorBeam : Reactor
+    {
+        public int TractorBeamLevel;
+        public TractorBeam()
+        {
+
+        }
+
+        public void Tractorbeamgeneratortower()
+        {
+            TractorBeamLevel = 0;
+            Console.WriteLine("The Generator is at" + TractorBeamLevel);
+        }
+
+        public void GenerateTractorBeam()
+        {
+            TractorBeamLevel++;
+        }
+
+        public void CreateForce(int power, int direction)
+        {
+            CreateForce = TractorBeamLevel + GenerateTractorBeam;
+            power = 10;
+            direction = right;
+
+        }
+    }
+}
